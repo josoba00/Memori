5cd414ca31ce1939bed4c5e5c057d4f5cff978eb
niko <niko@schennach.com>
2022-12-17 16:20:45 +0100

    changes to home and tests for deckbean

--- src/main/java/at/qe/skeleton/ui/beans/DeckBean.java
+++ src/main/java/at/qe/skeleton/ui/beans/DeckBean.java
@@ -1,5 +1,8 @@
 package at.qe.skeleton.ui.beans;
 
+import at.qe.skeleton.model.Deck;
+import at.qe.skeleton.model.DeckStatus;
+import at.qe.skeleton.model.User;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -11,8 +14,9 @@ import java.util.List;
 import java.util.Locale;
 
 /**
- * Viewscoped bean to retrieve decks.
- *
+ * Viewscoped bean to cache and retrieve Decks
+ * Also supplies a globalFilterFunction for Primefaces Datatable
+ * <p>
  * This class is part of the skeleton project provided for students of the
  * courses "Software Architecture" by the
  * University of Innsbruck.
@@ -21,52 +25,84 @@ import java.util.Locale;
 @ViewScoped
 public class DeckBean implements Serializable {
 
-    private List<Object> personalDecks;
-    private List<Object> savedDecks;
-    private List<Object> filteredPersonalDecks; // TODO: change to <Deck>
-    private List<Object> filteredSavedDecks; // TODO: change to <Deck>
+    private List<Deck> personalDecks;
+    private List<Deck> savedDecks;
+    private List<Deck> filteredPersonalDecks;
+    private List<Deck> filteredSavedDecks;
 
 
     @PostConstruct
-    public void init(){
-        this.personalDecks= new ArrayList<>(); //TODO: get decks from service
-        this.savedDecks= new ArrayList<>(); //TODO: get decks from service
+    public void init() {//TODO: get decks from service
+        this.personalDecks = new ArrayList<>();
+        this.savedDecks = new ArrayList<>();
+        Deck deck1 = new Deck();
+        Deck deck2 = new Deck();
+        User user = new User();
+        user.setUsername("john");
+        deck1.setDescription("deck-description 1");
+        deck1.setTitle("Deck1");
+        deck1.setStatus(DeckStatus.PUBLIC);
+        deck1.setCreator(user);
+        deck2.setDescription("deck-description 2Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.");
+        deck2.setTitle("Deck2");
+        deck2.setStatus(DeckStatus.PRIVATE);
+        personalDecks.add(deck1);
+        personalDecks.add(deck2);
+        savedDecks.add(deck1);
+        savedDecks.add(deck2);
+
     }
 
     public boolean globalFilterFunction(Object value, Object filter, Locale locale) {
         String filterText = (filter == null) ? null : filter.toString().trim().toLowerCase();
-        return true; // TODO: create globalFilterFunction
+        if (filter == null || filter == "") {
+            return true;
+        }
+        Deck deck = (Deck) value;
+        return deck.getTitle().toLowerCase().contains(filterText) || deck.getDescription().toLowerCase().contains(filterText);
     }
 
-    public List<Object> getPersonalDecks() {
+    public List<Deck> getPersonalDecks() {
         return personalDecks;
     }
 
-    public void setPersonalDecks(List<Object> personalDecks) {
+    public void setPersonalDecks(List<Deck> personalDecks) {
         this.personalDecks = personalDecks;
     }
 
-    public List<Object> getSavedDecks() {
+    public List<Deck> getSavedDecks() {
         return savedDecks;
     }
 
-    public void setSavedDecks(List<Object> savedDecks) {
+    public void setSavedDecks(List<Deck> savedDecks) {
         this.savedDecks = savedDecks;
     }
 
-    public List<Object> getFilteredPersonalDecks() {
+    public List<Deck> getFilteredPersonalDecks() {
         return filteredPersonalDecks;
     }
 
-    public void setFilteredPersonalDecks(List<Object> filteredPersonalDecks) {
+    public void setFilteredPersonalDecks(List<Deck> filteredPersonalDecks) {
         this.filteredPersonalDecks = filteredPersonalDecks;
     }
 
-    public List<Object> getFilteredSavedDecks() {
+    public List<Deck> getFilteredSavedDecks() {
         return filteredSavedDecks;
     }
 
-    public void setFilteredSavedDecks(List<Object> filteredSavedDecks) {
+    public void setFilteredSavedDecks(List<Deck> filteredSavedDecks) {
         this.filteredSavedDecks = filteredSavedDecks;
     }
+
+    public List<Deck> getDecks(int i) { //get list of cached decks associated to specific tab i
+
+        if (i == 0) {
+            return getPersonalDecks();
+        } else if (i == 1) {
+            return getSavedDecks();
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
 }
