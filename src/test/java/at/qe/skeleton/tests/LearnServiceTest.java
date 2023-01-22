b7c218e660f2725797f82c2646f8cbe8df16ab28
Lukas Kirchmair <csaw9392@student.uibk.ac.at>
2023-01-22 23:38:08 +0100

    Resolve "improve test coverage of LearnService and other beans"

--- src/test/java/at/qe/skeleton/tests/LearnServiceTest.java
+++ src/test/java/at/qe/skeleton/tests/LearnServiceTest.java
@@ -2,8 +2,9 @@ package at.qe.skeleton.tests;
 
 import at.qe.skeleton.model.*;
 import at.qe.skeleton.repositories.UserCardInfoRepository;
+import at.qe.skeleton.services.DeckService;
 import at.qe.skeleton.services.LearnService;
-import org.junit.Ignore;
+import at.qe.skeleton.services.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -12,14 +13,14 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.temporal.ChronoUnit;
-import java.util.Date;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
@@ -27,106 +28,128 @@ import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @WebAppConfiguration
-@Disabled
 class LearnServiceTest {
     @Autowired
-    LearnService mockLearnService;
+    LearnService learnService;
     @Mock
     UserCardInfoRepository mockUserCardInfoRepository;
+    @Autowired
+    DeckService deckService;
+    @Autowired
+    UserService userService;
+    @Autowired
+    UserCardInfoRepository userCardInfoRepository;
 
-    User testUser = new User();
-    Deck testDeck = new Deck();
-    Card testCard1 = new Card();
-    UserCardInfo testInfo1 = new UserCardInfo();
-    Card testCard2 = new Card();
-    UserCardInfo testInfo2 = new UserCardInfo();
-
-
-    @BeforeEach
-    void setUp(){
-        testUser.setUsername("testUser");
-
-        testCard1.setCardId(-10L);
-        testInfo1.setCard(testCard1);
-        testInfo1.setUser(testUser);
-        testInfo1.setRepetitionDate(new Date());
-        testInfo1.setEfFactor(2.5f);
-        testInfo1.setLearnInterval(1);
-
-        testCard2.setCardId(-11L);
-        testInfo2.setCard(testCard2);
-        testInfo2.setUser(testUser);
-        testInfo2.setRepetitionDate(Date.from(new Date().toInstant().plus(2, ChronoUnit.DAYS)));
-
-        testDeck.setContent(List.of(testCard1, testCard2));
-
-        mockLearnService = new LearnService();
-        ReflectionTestUtils.setField(mockLearnService, "userCardInfoRepository", mockUserCardInfoRepository);
-    }
-    
-    
-    
-    @Test
-    void findCardsToLearnTest(){
-        when(mockUserCardInfoRepository.findFirstByUserAndCard(testUser, testCard1)).thenReturn(testInfo1);
-        when(mockUserCardInfoRepository.findFirstByUserAndCard(testUser, testCard2)).thenReturn(testInfo2);
-
-        assertEquals(Set.of(testCard1), mockLearnService.findCardsToLearn(testDeck.getContent(), testUser));
-    }
 
     @Test
-    void findNeverLearnedCardTest(){
-        when(mockUserCardInfoRepository.findFirstByUserAndCard(testUser, testCard1)).thenReturn(testInfo1);
-        when(mockUserCardInfoRepository.findFirstByUserAndCard(testUser, testCard2)).thenReturn(null);
+    @Transactional
+    @WithMockUser(username = "admin", authorities = {"ADMIN"})
+    void findCardsToLearnTest() {
+        Iterator<User> userIterator = userService.getAllUsers().iterator();
+        boolean foundDeck = false;
+        User user = null;
+        Deck deck = null;
+        // Look for a deck that has enough cards to test correct order
+        while (userIterator.hasNext() && !foundDeck) {
+            user = userIterator.next();
+            Iterator<Deck> deckIterator = user.getCreatedDecks().iterator();
+            // Look through users deck to find a valid one
+            while (deckIterator.hasNext() && !foundDeck) {
+                deck = deckIterator.next();
+                if (deck.getContent().size() > 3) {
+                    foundDeck = true;
+                }
+            }
+        }
+        if (!foundDeck) throw new IllegalStateException("No deck with enough cars found for test");
+        List<Card> cardsInDeck = deck.getContent();
+        // Set date to today for every second card and remember those cards
+        // The others get the next date set to a few days later
+        long laterRepetitionTime = (new Date().getTime()) + 3 * 24 * 3600 * 1000;
+        Date laterRepetitionDate = new Date();
+        laterRepetitionDate.setTime(laterRepetitionTime);
 
-        assertEquals(Set.of(testCard2), mockLearnService.findNeverLearnedCards(testDeck.getContent(), testUser));
+        // Set repetition date accordingly for the test on all cards from the chosen deck
+        int cardNum = 0;
+        Set<Card> cardsToLearnToday = new HashSet<>();
+        for (Card card_i : cardsInDeck) {
+            UserCardInfo userCardInfo = userCardInfoRepository.findFirstByUserAndCard(user, card_i);
+            if (userCardInfo == null) {
+                learnService.updateUserCardInfo(card_i, user, 0);
+                userCardInfo = userCardInfoRepository.findFirstByUserAndCard(user, card_i);
+            }
+            Date chosenDate;
+            // Choose every 2nd card as to learn today
+            if (cardNum % 2 == 0) {
+                chosenDate = laterRepetitionDate;
+            } else {
+                chosenDate = new Date();
+                cardsToLearnToday.add(card_i);
+            }
+            cardNum++;
+            userCardInfo.setRepetitionDate(chosenDate);
+        }
+        Set<Card> toLearn = learnService.findCardsToLearn(cardsInDeck, user);
+        toLearn.addAll(learnService.findNeverLearnedCards(cardsInDeck, user));
+
+        assertEquals(cardsToLearnToday.size() ,toLearn.size());
+        for(Card card : toLearn){
+            assertTrue(cardsToLearnToday.contains(card));
+        }
     }
 
     @ParameterizedTest
-    @CsvSource({"0,0,1","0,1,1", "0,2,1",
-                "1,0,1","1,1,1","1,2,1",
-                "2,0,1","2,1,1","2,2,1",
-                "3,0,1","3,1,6","3,2,2",
-                "4,0,1","4,1,6","4,2,2"
-            })
-    void updateIntervalTest(int difficulty, int repetitions, int expected){
-        testInfo1.setNumberOfRepetitions(repetitions);
-        when(mockUserCardInfoRepository.findFirstByUserAndCard(testUser, testCard1)).thenReturn(testInfo1);
-
-       mockLearnService.updateUserCardInfo(testCard1, testUser, difficulty);
-
-
-        assertEquals(expected, testInfo1.getLearnInterval());
-
-    }
-
-    @ParameterizedTest
-    @CsvSource({"0,0,2.5","0,1,2.5", "0,2,2.5",
-                "1,0,2.5","1,1,2.5","1,2,2.5",
-                "2,0,2.5","2,1,2.5","2,2,2.5",
-                "3,0,2.5","3,1,2.5","3,2,2.36",
-                "4,0,2.5","4,1,2.5","4,2,2.5",
-                "5,0,2.5","5,1,2.5","5,2,2.6"
+    @CsvSource({"0,0,1", "0,1,1", "0,2,1",
+            "1,0,1", "1,1,1", "1,2,1",
+            "2,0,1", "2,1,1", "2,2,1",
+            "3,0,1", "3,1,6", "3,2,2",
+            "4,0,1", "4,1,6", "4,2,2"
     })
-    void updateEfFactorTest(int difficulty, int repetitions, float expected){
-        testInfo1.setNumberOfRepetitions(repetitions);
-        testInfo1.setEfFactor(2.5f);
-        when(mockUserCardInfoRepository.findFirstByUserAndCard(testUser, testCard1)).thenReturn(testInfo1);
+    @WithMockUser(username = "admin", authorities = {"ADMIN"})
+    @Transactional
+    void updateIntervalTest(int difficulty, int repetitions, int expected) {
+        User user = userService.getAllUsers().iterator().next();
+        Card card = deckService.getAllDecks().iterator().next().getContent().iterator().next();
+        // In case userCardInfo exists already we delete it first
+        UserCardInfo userCardInfo = userCardInfoRepository.findFirstByUserAndCard(user, card);
+        if (userCardInfo != null) {
+            userCardInfoRepository.delete(userCardInfo);
+        }
+        // Create userCardInfo to edit
+        learnService.updateUserCardInfo(card, user, difficulty);
+        userCardInfo = userCardInfoRepository.findFirstByUserAndCard(user, card);
+        userCardInfo.setNumberOfRepetitions(repetitions);
+        learnService.updateUserCardInfo(card, user, difficulty);
+        assertEquals(expected, userCardInfoRepository.findFirstByUserAndCard(user, card).getLearnInterval());
+    }
 
-        mockLearnService.updateUserCardInfo(testCard1, testUser, difficulty);
-
-
-        assertEquals(expected, testInfo1.getEfFactor());
+    @ParameterizedTest
+    @CsvSource({"0,0,2.5", "0,1,2.5", "0,2,2.5",
+            "1,0,2.5", "1,1,2.5", "1,2,2.5",
+            "2,0,2.5", "2,1,2.5", "2,2,2.5",
+            "3,0,2.5", "3,1,2.5", "3,2,2.36",
+            "4,0,2.5", "4,1,2.5", "4,2,2.5",
+            "5,0,2.5", "5,1,2.5", "5,2,2.6"
+    })
+    @WithMockUser(username = "admin", authorities = {"ADMIN"})
+    @Transactional
+    void updateEfFactorTest(int difficulty, int repetitions, float expected) {
+        UserCardInfo userCardInfo = userCardInfoRepository.findAll().iterator().next();
+        userCardInfo.setEfFactor(2.5f);
+        userCardInfo.setNumberOfRepetitions(repetitions);
+        learnService.updateUserCardInfo(userCardInfo.getCard(), userCardInfo.getUser(), difficulty);
+        assertEquals(expected, userCardInfo.getEfFactor());
     }
 
     @Test
-    void updateNumberRepetitionsTest(){
-        testInfo1.setNumberOfRepetitions(1);
-        when(mockUserCardInfoRepository.findFirstByUserAndCard(testUser, testCard1)).thenReturn(testInfo1);
-
-        mockLearnService.updateUserCardInfo(testCard1, testUser, 4);
-
-        assertEquals(2, testInfo1.getNumberOfRepetitions());
-
+    @WithMockUser(username = "admin", authorities = {"ADMIN"})
+    void updateNumberRepetitionsTest() {
+        UserCardInfo userCardInfo = userCardInfoRepository.findAll().iterator().next();
+        int oldRepetitionNum = userCardInfo.getNumberOfRepetitions();
+        User user = userCardInfo.getUser();
+        Card card = userCardInfo.getCard();
+        learnService.updateUserCardInfo(card, user, 2);
+        userCardInfo = userCardInfoRepository.findFirstByUserAndCard(user, card);
+        assertEquals(oldRepetitionNum + 1, userCardInfo.getNumberOfRepetitions());
     }
 }
